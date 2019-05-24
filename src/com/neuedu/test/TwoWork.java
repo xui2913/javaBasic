91b10a0b12d5cdcc8d3079cca9e294021f6e01bf
xui2913 <1468411573@qq.com>
2019-05-24 18:55:47 +0800

    New

---
+++ src/com/neuedu/test/TwoWork.java
@@ -0,0 +1,29 @@
+package com.neuedu.test;
+
+public class TwoWork {
+    public static void main(String[] args) {
+        int a,b,c,d,e,f,g;
+        for (g=100;g<1000;g++) {
+            for (a = 1; a < 10; a++) {
+                d = a * a * a;
+            }
+            for (b = 10; a < 100; b++) {
+                e = b * b * b;
+            }
+            for (c = 100; c < 1000; c++) {
+                f = c * c * c;
+            }
+            g='d'+'e'+'f';
+            System.out.println(g);
+
+//        int a;
+//        for (a=100;a<1000;a++){
+//            int b=a/100;
+//
+//
+//        }
+
+        }
+     }
+
+    }
