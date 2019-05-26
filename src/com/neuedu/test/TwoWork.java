c181fb634705ba06534417918b4edf1c4713b4c9
xui2913 <1468411573@qq.com>
2019-05-26 23:56:30 +0800

    New

--- src/com/neuedu/test/TwoWork.java
+++ src/com/neuedu/test/TwoWork.java
@@ -1,29 +1,65 @@
 package com.neuedu.test;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class TwoWork {
     public static void main(String[] args) {
-        int a,b,c,d,e,f,g;
-        for (g=100;g<1000;g++) {
-            for (a = 1; a < 10; a++) {
-                d = a * a * a;
-            }
-            for (b = 10; a < 100; b++) {
-                e = b * b * b;
-            }
-            for (c = 100; c < 1000; c++) {
-                f = c * c * c;
-            }
-            g='d'+'e'+'f';
-            System.out.println(g);
-
-//        int a;
+//        水仙花数
+//        int a,b,c,d;
 //        for (a=100;a<1000;a++){
-//            int b=a/100;
-//
-//
+//            b=a%10;
+//            c=a/10%10;
+//            d=a/100%10;
+//            if (a==b*b*b+c*c*c+d*d*d){
+//                System.out.println(a);
+//            }
 //        }
+//      学生成绩
+//        Scanner input = new Scanner(System.in);
+//        int result = input.nextInt();
+//         if (result>=90){
+//             System.out.println("A");
+//         } else if (result>59&&result<90){
+//             System.out.println("B");
+//         }else {
+//             System.out.println("C");
+//         }
+//   无重复数字
 
-        }
-     }
+//        Random rand = new Random();
+//        int n=0;
+//        for(int i = 1; i <= 4; ++i)
+//            for(int j = 1; j <= 4; ++j)
+//                for(int k = 1; k <= 4; ++k)
+//                    if(i != j && j != k && i != k && ++n != 0)
+//            System.out.println(i+""+j+""+k);
+//            System.out.println(rand.nextInt(432));
 
-    }
+
+//    9!
+//        int zhi=1;
+//        for (int shu=1;shu<10;shu++){
+//            zhi*=shu;
+//           }
+//        System.out.println(zhi);
+
+// 奇偶数和
+//        int jishuhe=0;
+//        int oushuhe=0;
+//    for (int shuzhi=1;shuzhi<=100;shuzhi++){
+//        if (shuzhi%2==0){
+//            jishuhe+=shuzhi;
+//        }else {
+//            oushuhe+=shuzhi;
+//        }
+//    }
+//        System.out.println("奇数和为："+jishuhe);
+//        System.out.println("偶数和为："+oushuhe);
+
+            }
+}
+
+
+
+
